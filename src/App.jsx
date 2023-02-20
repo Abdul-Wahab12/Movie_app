e830ca8e568cb62c693f57a6e345606edbf0d34f
Abdul Wahab <abdulwahabsoftware1@gmail.com>
2023-02-21 04:56:17 +0500

    Added: setup redux toolkit for store

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import './App.css'
 import { useEffect } from 'react';
 import {fetchDataFromApi} from './utils/api';
+import { useDispatch, useSelector } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
 
 function App() {
-
+  const dispatch = useDispatch();
+  const {url} = useSelector((state) => state.home);
+  console.log(url);
   useEffect(() => {
     apiTesting();
-  }, [])
+  }, []);
 
   const apiTesting = () => {
     fetchDataFromApi('/movie/popular').then((res) => {
       console.log(res);
-    })
-  }
+      dispatch(getApiConfiguration(res));
+    });
+  };
   return (
     <div className="App">
       App
+      {url?.total_pages}
     </div>
   )
 }
