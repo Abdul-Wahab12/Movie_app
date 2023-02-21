fa1cf1c6115b13b4616c7c16035144510d032927
Abdul Wahab <abdulwahabsoftware1@gmail.com>
2023-02-21 05:43:56 +0500

    Added: Create components and pages folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import { useEffect } from 'react';
 import {fetchDataFromApi} from './utils/api';
 import { useDispatch, useSelector } from 'react-redux';
 import { getApiConfiguration } from './store/homeSlice';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
+import PageNotFound from './pages/404/pageNotFound';
 
 function App() {
   const dispatch = useDispatch();
