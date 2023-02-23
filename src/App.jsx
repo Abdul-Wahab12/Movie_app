b53fadeb6c548eeb604c93a3e7636fd4085db8ec
Abdul Wahab <abdulwahabsoftware1@gmail.com>
2023-02-24 02:18:24 +0500

    Added: add header image banner section and update search input data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import { useEffect } from 'react';
 import {fetchDataFromApi} from './utils/api';
 import { useDispatch, useSelector } from 'react-redux';
@@ -16,20 +17,32 @@ function App() {
   const {url} = useSelector((state) => state.home);
   console.log(url);
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi('/movie/popular').then((res) => {
+  const fetchApiConfig = () => {
+    fetchDataFromApi('/configuration').then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res));
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      }
+      dispatch(getApiConfiguration(url));
     });
   };
   return (
-    <div className="App">
-      App
-      {url?.total_pages}
-    </div>
+    <BrowserRouter>
+    {/* <Header/> */}
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/:mediaType/:id' element={<Details/>}/>
+        <Route path='/search/:query' element={<SearchResult/>}/>
+        <Route path='/explore/:mediaType' element={<Explore/>}/>
+        <Route path='*' element={<PageNotFound/>}/>
+      </Routes>
+      {/* <Footer/> */}
+    </BrowserRouter>
   )
 }
 
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
-import './style.scss';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.scss";
+import { useState, useEffect } from "react";
+import useFetch from "../../../hooks/useFetch";
+import { useSelector } from "react-redux";
+import Img from "../../../components/lazyLoadImage/img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
-  return (
-    <div>HeroBanner</div>
-  )
-}
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+  // For Calling Api
+  const { data, loading } = useFetch("/movie/upcoming");
 
-export default HeroBanner
+  // set background image of header section
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      console.log(event);
+      navigate(`/search/${query}`);
+    }
+  };
+
+  return (
+    <div className="heroBanner">
+      {/* image of main header banner section */}
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
+
+      <div className="opacity-layer">
+        
+      </div>
+
+      <ContentWrapper>
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover. Explore now.
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie and tv show..."
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default HeroBanner;
